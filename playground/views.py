6005c7d1aa706b6f15d851bc9c42b290f7408bfa
Horlamide <olamide.jubril@taxtech.com.ng>
2021-12-24 23:01:08 +0100

    Working with Django  ORM for manipulating and querying data

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
-
-def calculate():
-    x = 1
-    y = 2
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': "Olamide"})
+    queryset = Product.objects.filter(unit_price__range=(20, 30))
+    
+    return render(request, 'hello.html', {'name': "Olamide", "products": list(queryset)})
--- store/models.py
+++ store/models.py
@@ -14,8 +14,9 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventories = models.IntegerField()
+    slug = models.CharField(max_length=255, default="-")
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
